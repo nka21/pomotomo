962c48145e9abb13a94825be2e3c2ae7a6883bd0
nka21 <naoki.nico@gmail.com>
2025-06-12 23:33:33 +0900

    delete: 現在あるディレクトリを削除

--- client/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-// tailwind.config.js
-module.exports = {
-    content: ["./index.html", "./src/**/*.{ts,tsx}"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-};
--- server/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { Hono } from 'hono'
-import { serve } from '@hono/node-server'
-
-const app = new Hono()
-
-app.get('/', (c) => c.text('Pomotomo API'))
-
-serve({ fetch: app.fetch, port: 3000 })
